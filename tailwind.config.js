df8fabd842421afb8bd96cc292fb12ba93d8267a
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-04-13 19:11:17 -0300

    custom tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'sidenav-gray': '#212124'
+      },
+      width:{
+        '14': '14%'
+      }
     },
   },
   plugins: [],
