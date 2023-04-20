609ae556ed2b9f0f112d6a228a569bcf1849bf3f
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-04-20 01:15:19 -0300

    feat: passing dinamic values by props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image"
-import cardImage from "../assets/img/image-thumb.png"
 
-export const Card = () => {
+export const Card = ({playlist}: {playlist: SpotifyApi.PlaylistObjectSimplified}) => {
     return (
         <a href="">
             <div className="h-80 w-52 py-2 drop-shadow-2xl rounded-3">
                 <div className="h-72">
                     <div className="w-full rounded-3">
-                        <Image src={cardImage} alt="card-image" width={208} height={208} className="rounded-3" />
+                        <Image src={playlist.images[0].url} alt="card-image" width={208} height={208} className="rounded-3" />
                     </div>
                     <div className="w-full h-20 pt-4">
-                        <h3 className="text-slate-50 font-semibold text-sm leading-5 mb-1">Blood Sugar Sex Magik</h3>
-                        <h4 className="text-gray-400 font-semibold text-xs leading-5">Red Hot Chili Peppers</h4>
-                        <span className="text-gray-400 font-semibold text-10 leading-3">LABEL</span>
+                        <h3 className="text-slate-50 font-semibold text-sm leading-5 mb-1">{playlist.name}</h3>
+                        <h4 className="text-gray-400 font-semibold text-xs leading-5 truncate">{playlist.description}</h4>
+                        <span className="text-gray-400 font-semibold text-10 leading-3">{playlist.type.toUpperCase()}</span>
                     </div>
                 </div>
             </div>
