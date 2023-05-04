ebf68be55953c61bd96c61520f78057d2c56a1eb
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-05-04 15:44:57 -0300

    feat: finished playlist detail page

--- src/components/PlaylistCel.tsx
+++ src/components/PlaylistCel.tsx
@@ -1,20 +1,33 @@
 import Image from "next/image"
-import testImage from "../assets/img/image-for-you.png"
 
-export const PlaylistCel = () => {
+type Props = {
+    playlist: SpotifyApi.PlaylistTrackObject | undefined,
+    index: number
+}
+
+export const PlaylistCel = ({playlist, index}: Props) => {
+
+    function msToMinutesAndSeconds(ms:number): string{
+        let min = Math.floor(ms /60000)
+        let sec = ((ms % 60000) / 1000).toFixed(0)
+        return `${min}:${sec}`
+    }
+    
+    let artists = playlist?.track?.artists.map((n) => n.name)
+
     return (
-        <tr className="tr">
-            <td>1</td>
+        <tr className="tr hover:bg-[#5c5c5c38] cursor-pointer">
+            <td>{index}</td>
             <td className="flex gap-2">
-                <Image className="" src={testImage} width={52} height={52} alt="" />
+                <Image className="" src={playlist?.track?.album?.images[0]?.url as string} width={52} height={52} alt="" />
                 <div>
-                    <p>Play It Safe</p>
-                    <p>Julia Wolf</p>
+                    <p>{playlist?.track?.name}</p>
+                    <p className="text-gray-400 mt-1">{artists?.join(", ")}</p>
                 </div>
             </td>
-            <td>Play It Safe</td>
-            <td> </td>
-            <td>2:12</td>
+            <td>{playlist?.track?.album.name}</td>
+            <td>{playlist?.added_at.slice(0, 10)}</td>
+            <td>{msToMinutesAndSeconds(playlist?.track?.duration_ms as number)}</td>
         </tr>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import { CategoriesPlaylistSection } from '@/components/CategoriesPlaylistsSection'
-import { FeatPlaylistSection } from '@/components/FeatPlaylistSection'
-import { SearchBar } from '@/components/SearchBar'
-import { SideBar } from '@/components/SideBar'
 import { Inter } from 'next/font/google'
-import { PlaylistDetail } from './playlistDetail'
+import { FeatPlaylistSection } from '@/components/FeatPlaylistSection'
+import { CategoriesPlaylistSection } from '@/components/CategoriesPlaylistsSection'
+import { SearchBar } from '@/components/SearchBar'
 
 const inter = Inter({
   weight: ['400', '500', '600', '700'],
@@ -13,17 +11,10 @@ const inter = Inter({
 export default function Home() {
   return (
     <main className={`${inter.className}`}>
-      <div className='flex bg-background'>
-        <SideBar />
-        <div className='w-containerSection overflow-y-scroll'>
-          <PlaylistDetail>
-            <SearchBar />
-          </PlaylistDetail>
-          {/*<FeatPlaylistSection/>
-          <CategoriesPlaylistSection categorie={"toplists"} name={"Top Lists"}/>
-<CategoriesPlaylistSection categorie={"in_the_car"} name={"To Listen in the Car"} />*/}
-        </div>
-      </div>
+      <SearchBar/>
+      <FeatPlaylistSection />
+      <CategoriesPlaylistSection categorie={"toplists"} name={"Top Lists"} />
+      <CategoriesPlaylistSection categorie={"in_the_car"} name={"To Listen in the Car"} />
     </main>
   )
 }
